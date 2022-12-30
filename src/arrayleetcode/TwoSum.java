1f9b6c2dedcd13a09824a0c11f9f64b1a526c225
karan <karang393@gmail.com>
2022-12-30 11:11:44 +0530

    containsDuplicate

---
+++ src/arrayleetcode/TwoSum.java
@@ -0,0 +1,28 @@
+//package arrayleetcode;
+//
+//public class TwoSum {
+//
+//    public static void main(String[] args) {
+//        Integer[] array = {2, 7, 3, 8};
+//
+//        Integer input = 9;
+//
+//        Result result = twoSum(array, input);
+//
+//        System.out.println("Result = " + result);
+//    }
+//
+//    private static Result twoSum(Integer[] array, Integer input) {
+//
+//        Integer current = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 1; j < array.length; j++) {
+//                current = array[i] + array[j];
+//                if (current == input) {
+//                    return (i);
+//                }
+//            }
+//        }
+//    }
+//
+//}
