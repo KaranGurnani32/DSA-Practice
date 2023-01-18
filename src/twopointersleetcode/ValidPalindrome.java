7db19d70a7fd5a7296f3ec5ec120bb5bcd3ca265
karan <karang393@gmail.com>
2023-01-18 19:02:11 +0530

    ValidPalindrome

---
+++ src/twopointersleetcode/ValidPalindrome.java
@@ -0,0 +1,34 @@
+package twopointersleetcode;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String input = "A man, a plan, a canal: Panama";
+
+        Boolean result = isPalindrome(input);
+        System.out.println("Result = " + result);
+    }
+
+    private static Boolean isPalindrome(String input) {
+        String tempString = "";
+
+        for (Character character : input.toCharArray()) {
+            if (Character.isDigit(character) || Character.isLetter(character)) {
+                tempString += character;
+            }
+        }
+        tempString = tempString.toLowerCase();
+
+        int leftPointer = 0;
+        int rightPointer = tempString.length() - 1;
+
+        while (leftPointer <= rightPointer) {
+            if (tempString.charAt(leftPointer) != tempString.charAt(rightPointer)) {
+                return false;
+            }
+
+            leftPointer++;
+            rightPointer--;
+        }
+        return true;
+    }
+}
